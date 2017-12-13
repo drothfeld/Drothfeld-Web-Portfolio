f8ec2d91c5754df1a8897d9b79969117343b7f1e
Dylan Rothfeld <drothfel@gmail.com>
2017-12-13 15:48:44 -0800

    expandable hamburger navigation menu

---
+++ Controllers/navigation-menu.js
@@ -0,0 +1,13 @@
+// Navigation Menu
+function openNavMenu() {
+  document.getElementById("main").style.marginLeft = "75%";
+  document.getElementById("mySidebar").style.width = "25%";
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
+}
+
+function closeNavMenu() {
+  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+}
