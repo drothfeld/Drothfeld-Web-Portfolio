9aca1a4d4eead0d05033135bf43dcecdb8d0f498
Dylan Rothfeld <drothfel@gmail.com>
2017-12-15 17:44:43 -0800

    enter animation for section nav controller

--- Controllers/navigation-menu.js
+++ Controllers/navigation-menu.js
@@ -1,15 +1,18 @@
 // Navigation Menu
 function openNavMenu() {
-  document.getElementById("main").style.marginLeft = "83%";
+  document.getElementById("main").style.marginRight = "17%";
   document.getElementById("mySidebar").style.width = "17%";
+  document.getElementById("mySidebar").style.marginLeft = "83%";
   document.getElementById("mySidebar").style.display = "block";
   document.getElementById("openNav").style.display = 'none';
+  document.getElementById("section-icons").style.marginLeft = "76.5%";
 }
 
 function closeNavMenu() {
-  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementById("main").style.marginRight = "0%";
   document.getElementById("mySidebar").style.display = "none";
   document.getElementById("openNav").style.display = "inline-block";
+  document.getElementById("section-icons").style.marginLeft = "93.5%";
 }
 
 // Easter Eggs
