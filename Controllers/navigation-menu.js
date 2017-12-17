adb63efc8b89c68993b999d3e6458e1501eaf7fa
Dylan Rothfeld <drothfel@gmail.com>
2017-12-17 15:51:52 -0800

    final project section struct

--- Controllers/navigation-menu.js
+++ Controllers/navigation-menu.js
@@ -5,12 +5,14 @@ function openNavMenu() {
   document.getElementById("mySidebar").style.marginLeft = "83%";
   document.getElementById("mySidebar").style.display = "block";
   document.getElementById("openNav").style.display = 'none';
-  document.getElementById("section-icons").style.marginLeft = "76.5%";
+  // document.getElementById("section-icons").style.marginLeft = "76.5%";
+  document.getElementById("section-icons").style.display = "none";
 }
 
 function closeNavMenu() {
   document.getElementById("main").style.marginRight = "0%";
   document.getElementById("mySidebar").style.display = "none";
   document.getElementById("openNav").style.display = "inline-block";
-  document.getElementById("section-icons").style.marginLeft = "93.5%";
+  // document.getElementById("section-icons").style.marginLeft = "93.5%";
+  document.getElementById("section-icons").style.display = "block";
 }
